f0b55c108d3aedc725f3aba87aaea78c8d34229c
origin <git@github.com:Azaharov14/helloworld.git>
2024-06-29 15:21:00 +0300

    4 commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,48 +1,175 @@
 import java.util.Objects;
-
+import static java.util.Objects.hash;
 public class Employee {
-    private String name;
-    private int salary;
+    private final String lastName;
+    private final String name;
+    private final String patronymic;
     private int department;
-    private static int id;
-    private static int nextId = 1;
+    private double salary;
+    private int id;
 
-    public Employee(String name, int salary, int department) {
+    public Employee(int id, String lastName, String name, String patronymic, int department, double salary) {
+        this.id = id;
+        this.lastName = lastName;
         this.name = name;
-        this.salary = salary;
+        this.patronymic = patronymic;
         this.department = department;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public int getSalary() {
-        return salary;
-    }
-    public int getDepartment() {
-        return department;
+        this.salary = salary;
     }
     public int getId() {
-        return id;
+        return this.id;
     }
-    public int setDepartment() {
-        return department;
+    public String getLastName() {
+        return this.lastName;
     }
-    public int setSalary() {
-        return salary;
+    public String getName() {
+        return this.name;
+    }
+    public String getPatronymic() {
+        return this.patronymic;
+    }
+    public int getDepartment() {
+        return this.department;
+    }
+    public void setDepartment(int numberDepartment) {
+        department = numberDepartment;
+    }
+    public double getSalary() {
+        return this.salary;
+    }
+    public void setSalary(double salaryOfWorker) {
+        salary = salaryOfWorker;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return salary == employee.salary && department == employee.department && Objects.equals(name, employee.name);
-    }
+    public String toString() {
 
+        System.out.print("id-" + this.id + " . ФИО: " + this.lastName + " " + this.name + " " + this.patronymic + " " + ". Номер отдела: " + this.department + ". Зарплата: " + this.salary);
+        return " ";
+    }
+    //печатаю список всех сотрудников со всеми полями
+    public static void printAll(Employee[] employees) {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i]);
+        }
+    }
+    public static double summaAllOnMonth(Employee[] employees) {
+        double summa = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > 0) {
+                summa += employees[i].getSalary();
+            }
+        }
+        System.out.println("\nСумма затрат на ЗП всех сотрудников в месяц составляет: " + summa);
+        return summa;
+    }
+    //Нахожу сотрудника с самой большой ЗП
+    public static void maxSummaOnMonthForEmployee(Employee[] employees) {
+        double summa = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > summa) {
+                summa = employees[i].getSalary();
+            }
+        }
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() == summa) {
+                System.out.print("В нашей компании, сотрудник с самой большой ЗП, является: ");
+                System.out.println((employees[i]));
+            }
+        }
+    }
+    //Нахожу сотрудника с самой низкой ЗП
+    public static void minSummaOnMonthForEmployee(Employee[] employees) {
+        double summa = employees[0].getSalary();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() < summa) {
+                summa = employees[i].getSalary();
+            }
+        }
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() == summa) {
+                System.out.print("В нашей компании, сотрудник с самой низкой ЗП, является: ");
+                System.out.println((employees[i]));
+            }
+        }
+    }
+    //считаем количество сотрдуников в компании
+    public static double numberOfEmployeesInTheCompany(Employee[] employees) {
+        double k = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                k++;
+            }
+        }
+        return k;
+    }
+    public static double averageSalary(Employee[] employees) {
+        double summa = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > 0) {
+                summa += employees[i].getSalary();
+            }
+        }
+        double k = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null) {
+                k++;
+            }
+        }
+        double averageSalary = summa / k;
+        System.out.println("Средняя ЗП по компании составляет: " + averageSalary);
+        return averageSalary;
+    }
+    //Для печати только полей ФИО из всех полей метода Employee
+    public static void printName(Employee[] employees) {
+        System.out.println("\nСписок всех сотрудников компании: ");
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println("ФИО: " + employees[i].lastName + " " + employees[i].name + " " + employees[i].patronymic);
+        }
+    }
     @Override
     public int hashCode() {
-        return Objects.hash(name, salary, department);
+        return hash(lastName, name, patronymic, department, salary);
     }
+    //Печатаем для каждого объекта Хэш код
+    public static void checkHash(Employee[] employees) {
+        System.out.print("\n Метод ХЭШ код: \n");
+        int id = 1;
+        for (int i = 0; i < employees.length; i++) {
+            System.out.print("id-" + id + ": ");
+            System.out.println(employees[i].hashCode());
+            id++;
+        }
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (other == null || this.getClass() != other.getClass()) {
+            return false;
+        }
+        Employee firstEmployees = (Employee) other;
+        boolean employeesComparison = ((lastName.equals(firstEmployees.lastName)) && (name.equals(firstEmployees.name)) && (patronymic == firstEmployees.patronymic));
+        return employeesComparison;
+    }
+    public static void equalsEmployee(Employee[] employees) {
+        System.out.print("\nCравним сотрудника id-1 со всеми остальными, через метод иквэлс, результат: \n");
+        int index = 0;
+        if (index >= 0 && index <= employees.length) {
+            for (int i = 0; i < employees.length; i++) {
+                if ((index != i) && (employees[index].equals(employees[i]) == true)) {
+                    System.out.println("\n_____Схожи сотрудники, смотри id: " + (index + 1) + " и смотри id: " + (i + 1));
+                    System.out.println(employees[index] + "_____\n");
 
+                } else if ((index != i) && (employees[index].equals(employees[i]) == false)) {
+                    System.out.print("Не найдено схожего сотрудника в id: ");
+                    System.out.println((i + 1));
+                }
+
+            }
+
+        } else {
+            throw new IllegalArgumentException("Индекс не может быть больше чем размер массива, и не может быть меньше 0");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final Employee[] employees = new Employee[10];
     public static void main(String[] args) {
+        System.out.println("Список всех сотрудников со всеми имеющимися полями: ");
+        employees[0] = new Employee(1, "Сергеев", "Иван", "Едорович", 1, 32_000);
+        employees[1] = new Employee(2, "Еремин", "Андрей", "Васильевич", 1, 10_000);
+        employees[2] = new Employee(3, "Кривобоков", "Роман", "Юрьевич", 2, 12_000);
+        employees[3] = new Employee(4, "Шевякова", "Анастасия", "Андреевна", 2, 14_000);
+        employees[4] = new Employee(5, "Камратов", "Андрей", "Валерьевич", 3, 122_000);
+        employees[5] = new Employee(6, "Евченко", "Андрей", "Вячеславович", 3, 22_000);
+        employees[6] = new Employee(7, "Лихачев", "Алексей", "Васильевич", 4, 21_000);
+        employees[7] = new Employee(8, "Бушмакина", "Ирина", "Александровна", 4, 40_000);
+        employees[8] = new Employee(9, "Иванова", "Наталья", "Викторовна", 5, 2_000);
+        employees[9] = new Employee(10, "Халявин", "Виктор", "Иванович", 5, 3_000);
 
-        static Employee[] employees = new Employee[10];
+        //печатаю список всех сотрудников со всеми полями
+        Employee.printAll(employees);
 
-        public static void main (String[]args){
+        //печатаю cумму затрат в месяц на ЗП всех сотрудников
+        Employee.summaAllOnMonth(employees);
 
-            employees[0] = createPeople("Заруцкий Алексей Семенович", 25000, 2);
-            employees[1] = createPeople("Авдеев Иосиф Александрович", 66000, 1);
-            employees[2] = createPeople("Фадеев Семен Сергеевич", 88800, 3);
-            employees[3] = createPeople("Захаров Роберт Александрович", 111100, 5);
-            employees[4] = createPeople("Варламова Анастасия Сергеевна", 34500, 4);
-            employees[5] = createPeople("Бузова Ольга Петровна", 99000, 2);
-            employees[6] = createPeople("Галустян Михаил Игоревич", 65740, 1);
-            employees[7] = createPeople("Пугачева Светлана Олеговна", 96430, 2);
-            employees[8] = createPeople("Медведев Лев Иннокентьевич", 34300, 5);
-            employees[9] = createPeople("Лососев Джек Воробеевич", 86230, 4);
+        //Нахожу самую Максимальную ЗП
+        Employee.maxSummaOnMonthForEmployee(employees);
 
-            printEmployees();
-        }
+        //Нахожу самую минимальную ЗП
+        Employee.minSummaOnMonthForEmployee(employees);
 
-        public static void printEmployees () {
-            for (Employee employee : employees) {
-                System.out.println(employee);
-            }
-        }
+        //Нахожу среднюю ЗП
+        Employee.averageSalary(employees);
+
+        //Печатаю ФИО всех сотрудников компании
+        Employee.printName(employees);
+
+        //Печатаем для каждого объекта Хэш код
+        Employee.checkHash(employees);
+
+        //Сравнение через иквэлс
+        Employee.equalsEmployee(employees);
     }
 }
